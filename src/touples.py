b9ad30032ab9428f4f6a346d54dd1bef2ac96ab3
Vedabahu <79869936+Vedabahu@users.noreply.github.com>
2023-09-08 06:52:05 +0100

    a thing about touples and dicts

---
+++ src/touples.py
@@ -0,0 +1,20 @@
+thing = ([1, 4, 5], [9, 1, 3])
+thing[0][2] = 999
+print(thing)
+
+# Dictionary
+
+monthConvertions = {
+    "jan": "January",
+    "feb": "Februrary",
+    "mar": "March",
+    "apr": "April",
+    "may": "May",
+    "jun": "June",
+    "jul": "July",
+    "aug": "August",
+    "spe": "September",
+    "oct": "October",
+    "nov": "November",
+    "dec": "December",
+}
