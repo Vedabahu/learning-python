ae3bdd0641c915761c5a38ba5e74d37aa6bd12fe
Vedabahu <79869936+Vedabahu@users.noreply.github.com>
2023-12-02 20:22:59 +0530

    Created initial datatypes file

---
+++ src/datatypes/datatypes.py
@@ -0,0 +1,32 @@
+# Strings
+string = "WTF?"
+
+# Integer
+
+x = 4
+
+# Float
+
+y = 3.14
+
+# Boolean
+
+z = True
+
+# List
+
+a = ["True", True, 1, 1.0]
+
+# Touple
+
+b = ("True", True, 1, 1.0)
+
+# Set
+
+sets = {1, 3, 4, 4, 1, 2, 5}  # {1, 2, 3, 4, 5} as there is no duplication
+print(sets)
+
+
+# Dictionary
+
+dictiomary = {"key": "value", "key2": "vales2"}
